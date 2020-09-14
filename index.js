9088ec295d652c9945b9cf9cf30b11cd6fc42900
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-14 23:22:28 +0300

    Done.

--- index.js
+++ index.js
@@ -3,28 +3,33 @@ class Cat {
     constructor(name){
         this.name=name;
     }
-    speak=()=>this.name+' says meow!';
+    speak(){
+       return this.name+' says meow!';
+    }
 }
 
 class Dog {
-    //...
+  
     constructor(name){
         this.name=name;
     }
-    speak=()=>this.name+' says woof!';
+    speak() {
+        return `${this.name} says woof!`
+      }
 }
 
 class Bird {
-    //...
-    constructor(gender,name){
+  
+    constructor(name,gender){
         this.gender=gender;
         this.name=name;
     }
-    speak=()=>{
-        if(this.gender=='male')
-        return ` It\'s me! ${this.name}, the parrot!`;
-        else
-        return ` ${this.name} says squawk!`;
-
+    speak(){
+    
+        if (this.gender == "male") {
+            return `It's me! ${this.name}, the parrot!`
+          } else {
+            return `${this.name} says squawk!`;
     }
   }
+}
