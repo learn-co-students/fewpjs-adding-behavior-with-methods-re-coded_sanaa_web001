90c5f6cd9c4ad3234fae2c9e55197950e7bc9c52
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-14 21:34:16 +0300

    finished tasks

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Cat {
+    constructor(name){
+        this.name=name;
+    }
+    speak=()=>this.name+' says meow!';
+}
+
+class Dog {
+    //...
+    constructor(name){
+        this.name=name;
+    }
+    speak=()=>this.name+' says woof!';
+}
+
+class Bird {
+    //...
+    constructor(gender,name){
+        this.gender=gender;
+        this.name=name;
+    }
+    speak=()=>{
+        if(this.gender=='male')
+        return ` It\'s me! ${this.name}, the parrot!`;
+        else
+        return ` ${this.name} says squawk!`;
+
+    }
+  }
